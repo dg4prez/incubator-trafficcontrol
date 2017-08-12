f1bc7acc115e77f0b365562006fbfcd1eaf9d2c3
Derek Gelinas <derek_gelinas@cable.comcast.com>
2017-08-12 08:42:14 -0400

    initial commit

--- traffic_ops/traffic_ops_golang/routes.go
+++ traffic_ops/traffic_ops_golang/routes.go
@@ -36,6 +36,7 @@ func Routes(d ServerData) ([]Route, http.Handler, error) {
 	return []Route{
 		{1.2, http.MethodGet, "cdns/{cdn}/configs/monitoring", wrapHeaders(wrapAuth(monitoringHandler(d.DB), d.Insecure, d.TOSecret, rd.PrivLevelStmt, MonitoringPrivLevel))},
 		{1.2, http.MethodGet, "cdns/{cdn}/configs/monitoring.json", wrapHeaders(wrapAuth(monitoringHandler(d.DB), d.Insecure, d.TOSecret, rd.PrivLevelStmt, MonitoringPrivLevel))},
+		{1.2, http.MethodGet, "servers/{host}/configs/ats/{filename}", wrapHeaders(wrapAuth(atsServerScopeConfigs(d.DB), d.Insecure, d.TOSecret, privLevelStmt, MonitoringPrivLevel))},
 	}, rootHandler(d), nil
 }
 
@@ -66,3 +67,4 @@ func rootHandler(d ServerData) http.Handler {
 	loggingProxyHandler := wrapAccessLog(d.TOSecret, rp)
 	return loggingProxyHandler
 }
+
